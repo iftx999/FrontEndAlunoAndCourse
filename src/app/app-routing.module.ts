c6cb72c5e4814bcd1c0d39f8a20022e0409811a4
Matheus <iftx159@gmail.com>
2023-03-02 09:01:30 -0300

    Ultimas implementaçoes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'dash' },
+  { path: '', pathMatch: 'full', redirectTo: 'app-dashboard' },
   { path: '', pathMatch: 'full', redirectTo: 'aluno' },
   { path: '', pathMatch: 'full', redirectTo: 'courses' },
 
   {
     
-    path: 'dash',
+    path: 'app-dashboard',
     loadChildren: () => import('./dashboard/dash.module').then(y => y.DashModule),
  
    
--- src/app/dashboard/dash.module.ts
+++ src/app/dashboard/dash.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import {MatGridListModule} from '@angular/material/grid-list';
 
-
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { SharedModule } from './../shared/shared.module';
 //import { CoursesListComponent } from './components/courses-list/courses-list.component'
