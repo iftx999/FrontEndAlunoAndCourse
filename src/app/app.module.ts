77aa2ff5a940a5e9e4fc645b09ca617a1d235990
matheus <matheus.henrique@elabbora.com.br>
2023-06-06 16:41:32 -0300

    Ultima alteração

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatCardModule } from '@angular/material/card';
@@ -7,32 +10,24 @@ import { AppMaterialModule } from './shared/app-material/app-material.module';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import { NavbarComponent } from './navbar/navbar.component';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgxMaskModule } from 'ngx-mask';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
     HttpClientModule,
     MatCardModule,
     AppMaterialModule,
     MatSidenavModule,
     MatListModule,
-    NgModule,
     NgxMaskModule,
-      
-    
-
     
     
   ],
