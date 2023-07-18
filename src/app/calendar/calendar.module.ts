662cf86acbdf5535bab6ab96ac470544d0d76cfc
matheus <matheus.henrique@elabbora.com.br>
2023-07-18 10:10:33 -0300

    Agenda: Corrige implementação de agenda

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -4,14 +4,13 @@ import { FullCalendarModule } from '@fullcalendar/angular';
 import { AppCalendar } from './app.calendar';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { CalendarComponent } from './calendar';
 
 
 
 const routes: Routes = [
   {
     path: '',
-    component: CalendarComponent
+    component: AppCalendar
   }
 ];
 
