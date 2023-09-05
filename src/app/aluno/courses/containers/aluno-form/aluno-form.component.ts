2289bef6c1f94de88bba9b10325ff2cc3b6d31b3
Matheus <iftx159@gmail.com>
2023-09-05 20:25:24 -0300

    Types: Correçao de tpagem no service

--- src/app/aluno/courses/containers/aluno-form/aluno-form.component.ts
+++ src/app/aluno/courses/containers/aluno-form/aluno-form.component.ts
@@ -20,12 +20,12 @@ export class AlunoFormComponent implements OnInit {
   isLoading: boolean = false;
 
   alunoForm = this.formBuilder.group({
-    idAluno: [''],
+    idAluno: [0],
     idade: [0],
     responsavel: [''],
     contato: [''],
     nome: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
-    cpf: ['', [Validators.required]],
+    cpf: [0, [Validators.required]],
     idCourse: [null, [Validators.required]]// Adicionado campo para o ID do curso
   });
 
@@ -41,7 +41,7 @@ export class AlunoFormComponent implements OnInit {
   ngOnInit(): void {
     const aluno: Aluno = this.route.snapshot.data['aluno'];
     this.alunoForm.setValue({
-      idAluno: aluno.idAluno !== null ? aluno.idAluno : '' as number, // Use a asserção de tipo aqui
+      idAluno: aluno.idAluno , // Use a asserção de tipo aqui
       nome: aluno.nome,
       contato: aluno.contato,
       responsavel: aluno.responsavel,
